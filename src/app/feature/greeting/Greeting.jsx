8d7d7b186ed1cf2ef0e109d7c0247a4f8e4820a2
sagafonoff <sandor.agafonoff@hellofresh.com.au>
2022-03-01 11:08:41 +1100

    test component with onchange example

--- src/app/feature/greeting/Greeting.jsx
+++ src/app/feature/greeting/Greeting.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import { greet } from "./greet";
 
-const Greeting = (props) => {
-  return <h1>{greet(props.personsName)}</h1>;
+const Greeting = () => {
+  // handles the state of the input
+  const [personsName, setPersonsName] = useState("");
+
+  // handles the greeting
+  const [greeting, setGreeting] = useState("");
+
+  const handleChange = (ev) => {
+    ev.preventDefault();
+    setPersonsName(ev.currentTarget.value);
+    setGreeting(greet(ev.currentTarget.value));
+  };
+
+  return (
+    <div>
+      <h1>{greeting}</h1>
+      <input
+        value={personsName}
+        aria-label="persons-name"
+        onChange={handleChange}
+      />
+    </div>
+  );
 };
 
 export { Greeting };
--- src/app/feature/greeting/Greeting.spec.jsx
+++ src/app/feature/greeting/Greeting.spec.jsx
@@ -1,7 +1,30 @@
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Greeting } from "./Greeting";
 
 describe("The behaviour of the Greeting component", () => {
   test("the greeting component is a function", () => {
     expect(typeof Greeting).toBe(typeof Function);
+  });
+
+  // test("the header contains the default greeting with no name provided", () => {
+  //   render(<Greeting />);
+
+  //   expect(screen.getByRole("heading")).toHaveTextContent("Hi!");
+  // });
+
+  // test("the header contains the greeting", () => {
+  //   render(<Greeting personsName="Fred Smith" />);
+
+  //   expect(screen.getByRole("heading")).toHaveTextContent("Hi! Fred");
+  // });
+});
+
+describe("given the person is on the greeting page, when they enter their name, then", () => {
+  it("displays the greeting on the page", () => {
+    const component = render(<Greeting />);
+    const nameInput = component.getByLabelText("persons-name");
+    fireEvent.change(nameInput, { target: { value: "Fred Smith" } });
+    expect(screen.getByRole("heading")).toHaveTextContent("Hi! Fred");
   });
 });
